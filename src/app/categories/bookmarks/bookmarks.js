a33d13bca1be6b53a14202e1877fa60a5c26ea83
tangzx <moumoon711@gmail.com>
2015-09-22 17:29:52 +0800

    init

---
+++ src/app/categories/bookmarks/bookmarks.js
@@ -0,0 +1,6 @@
+angular.module('categories.bookmarks', [
+    'categories.bookmarks.create',
+    'categories.bookmarks.edit',
+    'eggly.module.categories',
+    'eggly.module.bookmarks'
+]);
---
+++ src/app/categories/categories.js
@@ -0,0 +1,1 @@
+angular.module('categories', ['eggly.module.categories']);
